f01d7a52354db8b977e70c9d6efd3b188a0f5c2d
phantom101-IB <mi95920@gmail.com>
2024-06-12 15:54:04 +0000

    Updated Commit

--- controller/users.js
+++ controller/users.js
@@ -1,9 +1,37 @@
+const { StatusCodes } = require("http-status-codes")
+const User = require("../models/user-model")
+const {
+    notFoundError,
+    unAuthorizeError,
+    badRequestError,
+} = require("../errors")
+
 const register = async (req, res) => {
-    res.send("Phantom")
+    const user = await User.create({ ...req.body })
+    const token = await user.createJWT()
+    res.status(StatusCodes.CREATED).json({ name: user.name, token })
 }
 
 const login = async (req, res) => {
-    res.send("Login")
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new badRequestError("Please provide email and password")
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new notFoundError(`User with email: ${email} not found`)
+    }
+
+    const correctPassword = await user.comparePassword(password)
+    if (!correctPassword) {
+        throw new unAuthorizeError("Incorrect Password")
+    }
+
+    const token = await user.createJWT()
+
+    res.status(StatusCodes.OK).json({ name: user.name, token })
 }
 
 module.exports = { register, login }
--- routes/user-route.js
+++ routes/user-route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { register, login } = require("../controller/users")
 
-router.post("/", register)
-router.post("/", login)
+router.post("/register", register)
+router.post("/login", login)
 
 module.exports = router
